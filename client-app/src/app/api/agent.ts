ed1a7601e1a70d0e8a4af2cd709ac5fab922dd86
Zvonimir Korajlija <zvonimir.korajlija@gmail.com>
2020-05-22 17:46:10 +0200

    Added UI toastify notifications

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,7 +1,30 @@
 import Axios, { AxiosResponse } from 'axios';
 import { IActivity } from '../models/activity';
+import { history } from '../..';
+import { toast } from 'react-toastify';
 
 Axios.defaults.baseURL = 'http://localhost:5000/api/';
+
+Axios.interceptors.response.use(undefined, (error) => {
+  if (error.message === 'Network Error' && !error.response) {
+    toast.error("Network error - can't connect to API!");
+  }
+  const { status, data, config } = error.response;
+  if (status === 404) {
+    history.push('/notfound');
+  }
+  if (
+    status === 400 &&
+    config.method === 'get' &&
+    data.errors.hasOwnProperty('id')
+  ) {
+    history.push('/notfound');
+  }
+  if (status === 500) {
+    toast.error('Server error!');
+  }
+  throw error;
+});
 
 const responseBody = (response: AxiosResponse) => response.data;
 
