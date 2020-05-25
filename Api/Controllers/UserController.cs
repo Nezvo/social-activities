fde74fca04598d2b78fed1156ef64ab6d9db3762
Zvonimir Korajlija <zvonimir.korajlija@gmail.com>
2020-05-25 15:26:46 +0200

    Added user controller with handler

---
+++ Api/Controllers/UserController.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+using Application.User;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    public class UserController : BaseController
+    {
+        [HttpPost("login")]
+        public async Task<ActionResult<User>> Login(Login.Request request) => await Mediator.Send(request);
+    }
+}
---
+++ Application/User/Login.cs
@@ -0,0 +1,65 @@
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Errors;
+using Domain;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.User
+{
+    public class Login
+    {
+        public class Request : IRequest<User>
+        {
+            public string Email { get; set; }
+            public string Password { get; set; }
+        }
+
+        public class RequestValidator : AbstractValidator<Request>
+        {
+            public RequestValidator()
+            {
+                RuleFor(x => x.Email).NotEmpty();
+                RuleFor(x => x.Password).NotEmpty();
+            }
+        }
+
+        public class Handler : IRequestHandler<Request, User>
+        {
+            private readonly UserManager<AppUser> userManager;
+            private readonly SignInManager<AppUser> signInManager;
+
+            public Handler(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+            {
+                this.signInManager = signInManager;
+                this.userManager = userManager;
+            }
+
+            public async Task<User> Handle(Request request, CancellationToken cancellationToken)
+            {
+                var user = await userManager.FindByEmailAsync(request.Email);
+
+                if (user == null)
+                    throw new RestException(HttpStatusCode.Unauthorized);
+
+                var result = await signInManager.CheckPasswordSignInAsync(user, request.Password, false);
+
+                if (result.Succeeded)
+                {
+                    // TODO: generate token
+                    return new User
+                    {
+                        DisplayName = user.DisplayName,
+                        Token = "Token",
+                        UserName = user.UserName,
+                        Image = null
+                    };
+                }
+
+                throw new RestException(HttpStatusCode.Unauthorized);
+            }
+        }
+    }
+}
