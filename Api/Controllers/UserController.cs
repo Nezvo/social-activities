769fec3c214d0aab141af2bc4b2aaacefa284ba1
Zvonimir Korajlija <zvonimir.korajlija@gmail.com>
2020-05-25 22:14:25 +0200

    Get current user with token

--- Api/Controllers/UserController.cs
+++ Api/Controllers/UserController.cs
@@ -5,13 +5,17 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers
 {
-    [AllowAnonymous]
     public class UserController : BaseController
     {
+        [AllowAnonymous]
         [HttpPost("login")]
         public async Task<ActionResult<User>> Login(Login.Request request) => await Mediator.Send(request);
 
+        [AllowAnonymous]
         [HttpPost("register")]
         public async Task<ActionResult<User>> Register(Register.Request request) => await Mediator.Send(request);
+
+        [HttpGet]
+        public async Task<ActionResult<User>> CurrentUser() => await Mediator.Send(new CurrentUser.Request());
     }
 }
