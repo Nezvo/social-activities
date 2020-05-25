b78b51274a17bfaf82a9763429e01c57bcc0cc3a
Zvonimir Korajlija <zvonimir.korajlija@gmail.com>
2020-05-25 15:16:23 +0200

    Refactored to use base controller

--- Api/Controllers/ActivitiesController.cs
+++ Api/Controllers/ActivitiesController.cs
@@ -8,33 +8,25 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ActivitiesController : ControllerBase
+    public class ActivitiesController : BaseController
     {
-        private readonly IMediator mediator;
-        public ActivitiesController(IMediator mediator)
-        {
-            this.mediator = mediator;
-        }
-
         [HttpGet]
-        public async Task<ActionResult<List<Activity>>> List() => await mediator.Send(new ActivityList.Request());
+        public async Task<ActionResult<List<Activity>>> List() => await Mediator.Send(new ActivityList.Request());
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Activity>> Details(Guid id) => await mediator.Send(new ActivityDetails.Request { Id = id });
+        public async Task<ActionResult<Activity>> Details(Guid id) => await Mediator.Send(new ActivityDetails.Request { Id = id });
 
         [HttpPost]
-        public async Task<ActionResult<Unit>> Create(ActivityCreate.Request request) => await mediator.Send(request);
+        public async Task<ActionResult<Unit>> Create(ActivityCreate.Request request) => await Mediator.Send(request);
 
         [HttpPut("{id}")]
         public async Task<ActionResult<Unit>> Edit(Guid id, ActivityEdit.Request request)
         {
             request.Id = id;
-            return await mediator.Send(request);
+            return await Mediator.Send(request);
         }
 
         [HttpDelete("{id}")]
-        public async Task<ActionResult<Unit>> Delete(Guid id) => await mediator.Send(new ActivityDelete.Request { Id = id });
+        public async Task<ActionResult<Unit>> Delete(Guid id) => await Mediator.Send(new ActivityDelete.Request { Id = id });
     }
 }
