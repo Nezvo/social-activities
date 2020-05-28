9db1a19384c84c5f5043a0921b565566a5bdc988
Zvonimir Korajlija <zvonimir.korajlija@gmail.com>
2020-05-29 01:45:58 +0200

    Added ability to manage profile photos

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -3,6 +3,7 @@ import { IActivity } from '../models/activity';
 import { history } from '../..';
 import { toast } from 'react-toastify';
 import { IUser, IUserFormValues } from '../models/user';
+import { IProfile, IPhoto } from '../models/profile';
 
 Axios.defaults.baseURL = 'http://localhost:5000/api/';
 
@@ -49,6 +50,13 @@ const requests = {
   post: (url: string, body: {}) => Axios.post(url, body).then(responseBody),
   put: (url: string, body: {}) => Axios.put(url, body).then(responseBody),
   delete: (url: string) => Axios.delete(url).then(responseBody),
+  postForm: (url: string, file: Blob) => {
+    let formData = new FormData();
+    formData.append('File', file);
+    return Axios.post(url, formData, {
+      headers: { 'Content-type': 'multipart/form-data' },
+    }).then(responseBody);
+  },
 };
 
 const Activities = {
@@ -74,6 +82,10 @@ const User = {
 const Profiles = {
   get: (username: string): Promise<IProfile> =>
     requests.get(`/profiles/${username}`),
+  uploadPhoto: (photo: Blob): Promise<IPhoto> =>
+    requests.postForm(`/photos`, photo),
+  setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
+  deletePhoto: (id: string) => requests.delete(`/photos/${id}`),
 };
 
 export default {
