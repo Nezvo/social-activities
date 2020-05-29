67be1feab7a67e766cbdab1d82352256311ea313
Zvonimir Korajlija <zvonimir.korajlija@gmail.com>
2020-05-29 02:42:51 +0200

    Added profile description nad ability to edit DisplayName and Bio

--- Api/Controllers/ProfilesController.cs
+++ Api/Controllers/ProfilesController.cs
@@ -1,5 +1,6 @@
 using System.Threading.Tasks;
 using Application.Profiles;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers
@@ -8,5 +9,8 @@ namespace Api.Controllers
     {
         [HttpGet("{username}")]
         public async Task<ActionResult<Profile>> Get(string userName) => await Mediator.Send(new ProfileDetails.Query { UserName = userName });
+
+        [HttpPut]
+        public async Task<ActionResult<Unit>> Edit(ProfileEdit.Command command) => await Mediator.Send(command);
     }
 }
