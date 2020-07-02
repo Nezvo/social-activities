f80dde12c574896f88396cb21271c0670f95e675
Zvonimir Korajlija <zvonimir.korajlija@gmail.com>
2020-07-02 15:47:01 +0200

    Added token refresh endpoint with logic

--- Api/Controllers/UserController.cs
+++ Api/Controllers/UserController.cs
@@ -1,12 +1,26 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Application.User;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Api.Controllers
 {
     public class UserController : BaseController
     {
+        public IConfiguration Config { get; set; }
+
+        public UserController(IConfiguration config)
+        {
+            this.Config = config;
+        }
+
         [AllowAnonymous]
         [HttpPost("login")]
         public async Task<ActionResult<User>> Login(Login.Query request) => await Mediator.Send(request);
@@ -21,5 +35,35 @@ namespace Api.Controllers
         [AllowAnonymous]
         [HttpPost("facebook")]
         public async Task<ActionResult<User>> FacebookLogin(ExternalLogin.Query query) => await Mediator.Send(query);
+
+        [AllowAnonymous]
+        [HttpPost("refresh")]
+        public async Task<ActionResult<User>> Refresh(RefreshToken.Query query)
+        {
+            var principal = GetPrincipalFromExpiredToken(query.Token);
+            query.UserName = principal.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
+
+            return await Mediator.Send(query);
+        }
+
+        private ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
+        {
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Config["TokenKey"])),
+                ValidateLifetime = false
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out var securityToken);
+            var jwtSecurityToken = securityToken as JwtSecurityToken;
+            if (jwtSecurityToken == null || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha512, StringComparison.InvariantCultureIgnoreCase))
+                throw new SecurityTokenException("InvalidToken");
+
+            return principal;
+        }
     }
 }
