1c912679e030ee24a2c7a5589bebfb3ae56a0261
Zvonimir Korajlija <zvonimir.korajlija@gmail.com>
2020-09-30 00:09:54 +0200

    Added missing commit

--- Client/src/app/layout/App.tsx
+++ Client/src/app/layout/App.tsx
@@ -4,10 +4,10 @@ import NavBar from '../../features/nav/NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import { observer } from 'mobx-react-lite';
 import {
-  Route,
-  withRouter,
-  RouteComponentProps,
-  Switch,
+	Route,
+	withRouter,
+	RouteComponentProps,
+	Switch,
 } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
@@ -19,57 +19,67 @@ import LoadingComponent from './LoadingComponent';
 import ModalContainer from '../common/modals/ModalContainer';
 import ProfilePage from '../../features/profiles/ProfilePage';
 import PrivateRoute from './PrivateRoute';
+import RegisterSuccess from '../../features/user/RegisterSuccess';
+import VerifyEmail from '../../features/user/VerifyEmail';
 
 const App: React.FC<RouteComponentProps> = ({ location }) => {
-  const rootStore = useContext(RootStoreContext);
-  const { appLoaded, setAppLoaded, token } = rootStore.commonStore;
-  const { getUser } = rootStore.userStore;
+	const rootStore = useContext(RootStoreContext);
+	const { appLoaded, setAppLoaded, token } = rootStore.commonStore;
+	const { getUser } = rootStore.userStore;
 
-  useEffect(() => {
-    if (token) getUser().finally(() => setAppLoaded());
-    else setAppLoaded();
-  }, [token, getUser, setAppLoaded]);
+	useEffect(() => {
+		if (token) getUser().finally(() => setAppLoaded());
+		else setAppLoaded();
+	}, [token, getUser, setAppLoaded]);
 
-  if (!appLoaded) return <LoadingComponent content="Loading..." />;
+	if (!appLoaded) return <LoadingComponent content="Loading..." />;
 
-  return (
-    <Fragment>
-      <ModalContainer />
-      <ToastContainer position="bottom-right" />
-      <Route exact path="/" component={HomePage} />
-      <Route
-        path={'/(.+)'}
-        render={() => (
-          <Fragment>
-            <NavBar />
-            <Container style={{ marginTop: '7em' }}>
-              <Switch>
-                <PrivateRoute
-                  exact
-                  path="/activities"
-                  component={ActivityDashboard}
-                />
-                <PrivateRoute
-                  path="/activities/:id"
-                  component={ActivityDetails}
-                />
-                <PrivateRoute
-                  key={location.key}
-                  path={['/createActivity', '/manage/:id']}
-                  component={ActivityForm}
-                />
-                <PrivateRoute
-                  path="/profile/:username"
-                  component={ProfilePage}
-                />
-                <Route component={NotFound} />
-              </Switch>
-            </Container>
-          </Fragment>
-        )}
-      />
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<ModalContainer />
+			<ToastContainer position="bottom-right" />
+			<Route exact path="/" component={HomePage} />
+			<Route
+				path={'/(.+)'}
+				render={() => (
+					<Fragment>
+						<NavBar />
+						<Container style={{ marginTop: '7em' }}>
+							<Switch>
+								<PrivateRoute
+									exact
+									path="/activities"
+									component={ActivityDashboard}
+								/>
+								<PrivateRoute
+									path="/activities/:id"
+									component={ActivityDetails}
+								/>
+								<PrivateRoute
+									key={location.key}
+									path={['/createActivity', '/manage/:id']}
+									component={ActivityForm}
+								/>
+								<PrivateRoute
+									path="/profile/:username"
+									component={ProfilePage}
+								/>
+								<PrivateRoute
+									path="/user/registerSuccess"
+									component={RegisterSuccess}
+								/>
+								<PrivateRoute
+									path="/user/verifyEmail"
+									component={VerifyEmail}
+								/>
+								<Route component={NotFound} />
+							</Switch>
+						</Container>
+					</Fragment>
+				)}
+			/>
+		</Fragment>
+	);
 };
 
 export default withRouter(observer(App));
